845b69d6efad08c843dac678518edfd7bc8bc962
Hoang Kiem <kiemhd@outlook.com>
2024-02-24 00:40:36 +0700

    feat: database seeding

--- database/factories/ProductVariantFactory.php
+++ database/factories/ProductVariantFactory.php
@@ -17,7 +17,8 @@ class ProductVariantFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'color' => $this->faker->randomElement(['red', 'blue', 'green', 'orange', 'black', 'white', 'gray', 'purple', 'orange', 'pink', 'brown', 'beige']),
+            'size' => $this->faker->randomElement(['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL', 'XXXXL', 'XXXXXL']),
         ];
     }
 }
